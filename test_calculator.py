d273cbbb7a99039dbb17ca9826703c78bfc493e9
Samoresh2007 <samoreshdey76@gmail.com>
2025-06-29 09:56:56 +0530

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,13 +1,17 @@
 from calculator_logic import evaluate_expression
 
+
 def test_addition():
     assert evaluate_expression("1+2") == "3"
+
 
 def test_divide_by_zero():
     assert evaluate_expression("1/0") == "Error"
 
+
 def test_invalid_expression():
     assert evaluate_expression("2+") == "Error"
+
 
 def test_float_operation():
     assert evaluate_expression("3.5 * 2") == "7.0"
