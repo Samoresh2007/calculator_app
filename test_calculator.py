4532a2e5334144c688bbd76500e5f552b2963db9
Samoresh2007 <samoreshdey76@gmail.com>
2025-06-29 09:50:36 +0530

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,13 @@
-def evaluate_expression(expression):
-    try:
-        return str(eval(expression))
-    except:
-        return "Error"
+from calculator_logic import evaluate_expression
+
+def test_addition():
+    assert evaluate_expression("1+2") == "3"
+
+def test_divide_by_zero():
+    assert evaluate_expression("1/0") == "Error"
+
+def test_invalid_expression():
+    assert evaluate_expression("2+") == "Error"
+
+def test_float_operation():
+    assert evaluate_expression("3.5 * 2") == "7.0"
